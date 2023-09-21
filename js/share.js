685c9aa6830be1c813f41dc4bbf09ef8431b88f3
enddl3788 <enddl3788@naver.com>
2023-09-21 22:20:56 +0900

    테스트 14

--- js/share.js
+++ js/share.js
@@ -5,12 +5,12 @@ Kakao.isInitialized(); // SDK 초기화 여부를 리턴합니다.
 const url = 'https://enddl3788.github.io/dog-personality-profile-test/';
 
 function setShare() {
-    var resultImg = document.createElement('img');
+    var resultImg = document.createElement('#resultImg');
     var resultAlt = resultImg.firstElementChild.alt;
     const shareTitle = '반려견 성격 프로필 테스트 결과'
     const shareDes = infoList[resultAlt].name;
-    const shareImage = url + 'img/image-' + resultAlt + '.jpeg';
-    const shareURL = url + 'page.result-' + resultAlt + '.html';
+    const shareImage = url + 'img/image-' + resultAlt + '.jpeg';    
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
 
     Kakao.Link.sendDefault({
         //container: '#kakaotalk-sharing-btn',
@@ -27,7 +27,7 @@ function setShare() {
 
         buttons: [
         {
-            title: '결과 확인하기',
+            title: '결과확인하기',
             link: {
                 mobileWebUrl: shareURL,
                 webUrl: shareURL,
